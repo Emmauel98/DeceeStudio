75b588fe6f5b76f3ead0067e9589e95afbc5a744
emmanuel98 <salaudeenoluwapelumi98@gmail.com>
2024-07-25 16:21:29 +0100

    pushing react files

---
+++ src/Pages/Landing_Page/Herosection/Herosection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Herosection = () => {
+  return (
+    <div className='bg-green-400'>Herosection</div>
+  )
+}
+
+export default Herosection;
---
+++ src/Pages/Landing_Page/IndexPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import Herosection from '../Herosection/Herosection';
+import LandingPageBg from '../Images/LandingPageBg.svg';
+
+const LandingPage = () => {
+  return (
+    <>
+    <div className=' bg-[url("React_Template\src\Pages\Landing_Page\IndexPage\Land.svg")] '>
+        <Herosection />
+    </div>
+    </>
+  )
+}
+
+export default LandingPage;
